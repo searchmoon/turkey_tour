53031e3ab32662685d776caf7c232ca962afdd08
searchmoon <moonding901212@gmail.com>
2022-04-14 17:06:09 +0900

    UI 변경 및 개선

--- main.js
+++ main.js
@@ -1,33 +1,68 @@
 const header = document.querySelector("header");
-const signIn = document.querySelector(".sign_in");
 
-signIn.addEventListener("click", () => {
-  window.scrollTo({top: 200, left: 0, behavior: "smooth"});
-  
-  console.log("메롱");
-});
-console.log('왜지');
-
-
-
-
-let headerMoving = (direction) => {
-  if (direction === "up"){
-    console.log('올려');
-  } else if (direction === "down"){
-    console.log('내려');
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 200) {
+    header.style.top = "-100px";
+    // header.style.transition = "3s, ease-in-out";
+  } else {
+    header.style.top = "0";
   }
-};
-
-let prevScrollTop = 0;
-document.addEventListener("scroll", function(){
-  let nextScrollTop = window.pageYOffset || 0; // pageYOffset -> IE 8 이하 빼고 다 됨.
-  if (nextScrollTop > prevScrollTop){
-    headerMoving("down");
-    header.classList.add = "hidden";
-  } else if (nextScrollTop < prevScrollTop){
-    headerMoving("up");
-    header.classList.remove = "hidden";
-  }
-  prevScrollTop = nextScrollTop;
 });
+
+//로그인 기능(임시처리)
+
+const btnLogin = document.querySelector(".btn-login");
+const inpId = document.querySelector("#inpId");
+const inpPw = document.querySelector("#inpPw");
+
+//1. 요소를 querySelector를 통해서 이벤트리스너 부여
+btnLogin.addEventListener("click", () => {
+  const data = [
+    { id: "banana", pw: "1212" },
+    { id: "mango", pw: "0000" },
+    { id: "mangu", pw: "1313" },
+  ];
+
+  for (let i = 0; i < data.length; i++) {
+    if (inpId.value === data[i].id && inpPw.value === data[i].pw) {
+      console.log("로그인 완료");
+      return;
+    }
+  }
+  console.log("로그인 실패");
+});
+
+//2. 함수 선언문
+// function loginButton() {
+//   const data = [
+//     { id: "banana", pw: "1212" },
+//     { id: "mango", pw: "0000" },
+//     { id: "mangu", pw: "1313" },
+//   ];
+
+//   for (let i = 0; i < data.length; i++) {
+//     if (inpId.value === data[i].id && inpPw.value === data[i].pw) {
+//       console.log("로그인 완료");
+//       return;
+//     }
+//   }
+//   console.log("로그인 실패");
+// }
+
+//3. 함수 표현식
+// const loginButton = function () {
+//   const data = [
+//     { id: "banana", pw: "1212" },
+//     { id: "mango", pw: "0000" },
+//     { id: "mangu", pw: "1313" },
+//   ];
+
+//   for (let i = 0; i < data.length; i++) {
+//     if (inpId.value === data[i].id && inpPw.value === data[i].pw) {
+//       console.log("로그인 완료");
+//       return;
+//     }
+//   }
+//   console.log("로그인 실패");
+// };
+// 함수선언문은 호이스팅에 영향을 받지만, 함수표현식은 호이스팅에 영향을 받지 않는다.
